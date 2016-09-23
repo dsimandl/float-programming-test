a163fa11c9425c0dad176024153900c1cf88d1e7
David Simandl <david.simandl@gmail.com>
2016-09-22 20:32:33 -0700

    fizzBuzz!

--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -17,5 +17,17 @@
  * 
  */
 module.exports = ( length ) => {
-
+    let result = new Array();
+    for(let i=1; i <= length; i++) {
+        if((i % 3 == 0) && (i % 5 == 0)) {
+            result.push('fizzbuzz');
+        } else if (i % 5 == 0) {
+            result.push('buzz');
+        } else if (i % 3 == 0) {
+            result.push('fizz');
+        } else {
+            result.push(i);
+        }
+    }
+    return result;
 };
