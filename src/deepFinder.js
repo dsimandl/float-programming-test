f53b4f09686a3b97c2f51ac6caa5a0a953cecbdc
David Simandl <david.simandl@gmail.com>
2016-09-22 20:29:51 -0700

    deepFinder.js

--- src/deepFinder.js
+++ src/deepFinder.js
@@ -12,7 +12,22 @@
  *   test: value => /^a/i.test( value )
  *   returns: [ 'ant', 'apple', 'aardvark', 'allegory' ]
  * 
- */
-module.exports = ( input, test ) => {
-
+ */ 
+const deepFinder = ( input, test, matches=[] ) => {
+    for(let val of input) {
+        if(typeof val === 'string' && test(val)) {
+            matches.push(val)
+        }
+        if(val.isArray) {
+            deepFinder(val, test, matches);
+        }
+        if(typeof val === 'object') {
+            for(let prop in val) {
+                deepFinder([val[prop]], test, matches);
+            }
+        }
+    }
+    return matches;
 };
+
+module.exports = deepFinder;
